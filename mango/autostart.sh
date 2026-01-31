998fa9fceb8e95e9f0be61f13218babc63278a3d
jens-buehnen <jenslmao@proton.me>
2026-01-31 14:43:21 +0100

    update

--- mango/autostart.sh
+++ mango/autostart.sh
@@ -2,7 +2,7 @@
 
 waybar &
 wl-paste --watch cliphist store &
-steam -silent &
+steam -silent -noverifyfiles &
 /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
 vesktop -m &
 swaybg -i $(fd wallpaper --search-path $(xdg-user-dir PICTURES) | head -n 1) &
