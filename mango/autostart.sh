52e1472ed60d764b3a6df8d27ecbef3c195e1813
jens-buehnen <jensLMAO@proton.me>
2026-02-12 15:04:14 +0100

    update

--- mango/autostart.sh
+++ mango/autostart.sh
@@ -1,9 +1,7 @@
 #/usr/bin/env bash
 
-waybar &>/dev/null
-wl-paste --watch cliphist store &>/dev/null
-steam -silent &>/dev/null
-/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &>/dev/null
-vesktop -m &>/dev/null
-swaybg -i $(fd wallpaper --search-path $(xdg-user-dir PICTURES) | head -n 1) &>/dev/null
-gio launch ~/.config/autostart/* &>/dev/null
+waybar >/dev/null
+wl-paste --watch cliphist store >/dev/null
+/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 >/dev/null
+swaybg -i $(fd wallpaper --search-path $(xdg-user-dir PICTURES) | head -n 1) >/dev/null
+gio launch ~/.config/autostart/* >/dev/null
