1844b888b2f895c4897b3f70703cd24dfcf41ba8
jens-buehnen <jensLMAO@proton.me>
2026-02-25 15:07:47 +0100

    update

--- mango/autostart.sh
+++ mango/autostart.sh
@@ -4,4 +4,6 @@ waybar &
 wl-clip-persist --clipboard both &
 /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
 swaybg -i $(fd wallpaper --search-path $(xdg-user-dir PICTURES) | head -n 1) &
-gio launch ~/.config/autostart/* &
+for i in ~/.config/autostart/; do
+	gio launch $i &
+done
