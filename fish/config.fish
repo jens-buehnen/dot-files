01396cdfaaf6ee93d02b0b98b3306f29befb28f4
jens-buehnen <jenslmao@proton.me>
2026-02-27 16:13:53 +0100

    update

--- fish/config.fish
+++ fish/config.fish
@@ -6,4 +6,5 @@ if status is-interactive
     end
 end
 zoxide init fish | source
+fish_config theme choose "Catppuccin Mocha" --color-theme=dark
 tide configure --auto --style=Lean --prompt_colors='16 colors' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Compact --icons='Few icons' --transient=Yes
