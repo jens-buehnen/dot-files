39b4ab56f7a8352577b572c78e3c0a21c628f094
jens-buehnen <jenslmao@proton.me>
2026-02-27 15:52:13 +0100

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,9 @@
-source ~/.config/fish/conf.d/*
 set fish_greeting
 bind ctrl-f zi
-bind ctrl-r _fzf_search_history
 if status is-interactive
     if command -v hyfetch >/dev/null
         hyfetch
     end
 end
 zoxide init fish | source
-starship init fish | source
+tide configure --auto --style=Lean --prompt_colors='16 colors' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Compact --icons='Few icons' --transient=Yes
