36ff4dc9bdd362da5e2fac1d5d15fdc00e916f63
jens-buehnen <jenslmao@proton.me>
2026-03-23 07:11:17 +0100

    update

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,6 @@ zoxide init fish | source
 fish_config theme choose catppuccin-mocha --color-theme=dark
 tide configure --auto --style=Lean --prompt_colors='16 colors' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Compact --icons='Few icons' --transient=Yes
 if status is-interactive
-    clear
     if command -v hyfetch >/dev/null
         hyfetch
     end
