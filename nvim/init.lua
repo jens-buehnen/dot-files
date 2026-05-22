67454bcee6be1a739e9a4bd6366d1aa6cbc87461
jens-buehnen <jensLMAO@proton.me>
2026-05-22 17:41:33 +0200

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,11 @@
-require("config.lazy")
+require("pack")
 vim.cmd.colorscheme("catppuccin")
 require("mini.cmdline").setup()
 require("mini.tabline").setup()
 require("mini.snippets").setup()
 require("mini.comment").setup()
 require("mini.cursorword").setup()
-require("mini.map").setup({
-	window = {
-		focusable = true,
-	},
-})
-MiniMap.open()
-require("oil").setup()
-require("modicator").setup()
+require("mini.map").setup()
 require("lualine").setup()
 require("conform").setup({
 	formatters_by_ft = {
@@ -28,31 +21,49 @@ require("conform").setup({
 		},
 	},
 })
-
 local hipatterns = require("mini.hipatterns")
 hipatterns.setup({
 	highlighters = {
-		-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
 		fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
 		hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
 		todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
 		note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-
-		-- Highlight hex color strings (`#rrggbb`) using that color
 		hex_color = hipatterns.gen_highlighter.hex_color(),
 	},
 })
-
 vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*",
 	callback = function(args)
 		require("conform").format({ bufnr = args.buf })
 	end,
 })
-
 vim.keymap.set("n", "<Leader>mc", MiniMap.close)
 vim.keymap.set("n", "<Leader>mf", MiniMap.toggle_focus)
 vim.keymap.set("n", "<Leader>mo", MiniMap.open)
 vim.keymap.set("n", "<Leader>mr", MiniMap.refresh)
 vim.keymap.set("n", "<Leader>ms", MiniMap.toggle_side)
 vim.keymap.set("n", "<Leader>mt", MiniMap.toggle)
+vim.o.cursorline = true
+vim.o.number = true
+vim.o.termguicolors = true
+vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+	name = "wl-clipboard",
+	copy = {
+		["+"] = "wl-copy --foreground --type text/plain",
+		["*"] = "wl-copy --foreground --primary --type text/plain",
+	},
+	paste = {
+		["+"] = function()
+			return vim.fn.systemlist("wl-paste --no-newline", { "" }, 1)
+		end,
+		["*"] = function()
+			return vim.fn.systemlist("wl-paste --primary --no-newline", { "" }, 1)
+		end,
+	},
+	cache_enabled = true,
+}
+vim.api.nvim_create_user_command("PackUpdate", function()
+	vim.pack.update()
+end, { desc = "Update all vim.pack plugins" })
+MiniMap.open()
---
+++ nvim/lua/pack.lua
@@ -0,0 +1,7 @@
+vim.pack.add({
+	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
+	{ src = "https://github.com/nvim-mini/mini.nvim", version = "stable" },
+	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
+	{ src = "https://github.com/stevearc/conform.nvim" },
+	{ src = "https://github.com/folke/which-key.nvim" },
+})
